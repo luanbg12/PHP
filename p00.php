11ac6e62d9f52f34c190f6416ba7884a40dc157d
Luan Braga <126211492+luanbg12@users.noreply.github.com>
2023-07-24 19:12:23 -0300

    Create p00.php

---
+++ p00.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "teste de mensagem em HP";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Uma mensagem formatada em hp
+
+
+</body>
+</html>
