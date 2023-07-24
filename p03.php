dd0e66a1b33c0d417b3f1adb1ab9d4bdb565fbe0
Luan Braga <126211492+luanbg12@users.noreply.github.com>
2023-07-24 19:14:08 -0300

    Create p03.php

---
+++ p03.php
@@ -0,0 +1,12 @@
+<?php
+if (5 < 2){
+    echo "deu certo! Entrou no If <br>";
+} else{
+    echo "deu certo! Entrou no else <br>";
+}
+
+$x = 10;
+$y = 15;
+$media = $x + $y;
+
+echo "O resultado é $media!";
