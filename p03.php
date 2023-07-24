f428998a404f29b84e9d227cf0f397ad4261c6b4
Luan Braga <126211492+luanbg12@users.noreply.github.com>
2023-07-24 19:13:25 -0300

    Create p03.php

---
+++ p03.php
@@ -0,0 +1,26 @@
+<?php
+
+/*Calculos +-*/
+
+$n1 = 10;
+$n2 = 15;
+$somar = $n1 + $n2;
+$subtrair = $n1 - $n2;
+$multiplicar = $n1 * $n2;
+$dividir = $n1 / $n2;
+$resto = $n2 % $n1;
+$potencia =  2 ** 2;
+$Potencia = 2 **3;
+
+echo "A soma é: $somar";
+echo "<br>";
+echo "A subtração é: $subtrair";
+echo "<br>";
+echo "A multiplicação é: $multiplicar";
+echo "<br>";
+echo "A divisão é: $dividir";
+echo "<br>";
+echo "O Resto é $resto";
+echo "<br>";
+echo "A potencia é $Potencia";
+echo "<br>";
