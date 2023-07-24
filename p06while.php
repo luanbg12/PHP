eb3b0d92c441e156ac8a09c4f25a01440b1988eb
Luan Braga <126211492+luanbg12@users.noreply.github.com>
2023-07-24 19:15:01 -0300

    Create p06while.php

---
+++ p06while.php
@@ -0,0 +1,15 @@
+<?php 
+
+$i = 4;
+$tab= 2;
+$x= 1;
+$resultado = 0;
+$mult = 10;
+
+while ($x <= 10){
+    $resultado = $tab * $x;
+    echo "$tab x $x = $resultado <br>";
+    $x++;
+}
+
+echo "Saindo do While";
